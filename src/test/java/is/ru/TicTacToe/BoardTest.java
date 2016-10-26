fdda28feeb67c5c7d259c329af84162cddddc9a3
astabergs <astab14@ru.is>
2016-10-26 22:38:43 +0000

    test for empty board

--- src/test/java/is/ru/TicTacToe/BoardTest.java
+++ src/test/java/is/ru/TicTacToe/BoardTest.java
@@ -3,12 +3,15 @@ package is.ru.TicTacToe;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class BoardTest
-{
-		@Test 
-		public void testBoardClass()
-		{
-			Board classToTest = new Board();
-			assertNotNull(classToTest);
-		}
+public class BoardTest {
+	@Test 
+	public void testNewBoard() {
+		
+		//int board[][] = new int [3][3];
+
+		Board b = new Board();
+
+		assertEquals(true, b.isEmpty());
+
+	}
 }
