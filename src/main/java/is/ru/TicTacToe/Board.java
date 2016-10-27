fac5eccec765102bbf929bd51c623f7728be9125
olafur <olafuralbert@gmail.com>
2016-10-27 22:03:48 +0000

    refactord Boardtest and Board

--- src/main/java/is/ru/TicTacToe/Board.java
+++ src/main/java/is/ru/TicTacToe/Board.java
@@ -1,8 +1,75 @@
 package is.ru.TicTacToe;
 
-public class Board{
-	
-	public Board(){
+
+import java.io.PrintWriter;
+
+public class Board {
+
+	private int ROW_SIZE = 3;
+	private int COLUMN_SIZE = 3;
+	public char board[][] = new char[ROW_SIZE][COLUMN_SIZE];
+
+
+	public Board() {
+
+		for (int i = 0; i < ROW_SIZE; i++) {
+			for (int j = 0; j < COLUMN_SIZE; j++) {
+				board[i][j] = ' ';
+			}
+		}	
 
 	}
+
+	public boolean isEmpty() {
+		
+		for (int i = 0; i < ROW_SIZE; i++) {
+			for (int j = 0; j < COLUMN_SIZE; j++) {
+				if (board[i][j] != ' ') {
+					return false;
+				}
+			}	
+		}
+
+		return true;
+	}
+
+	public boolean checkIfMoveIsLegal(int r, int c) {
+		
+		if ((board[r][c]==' ' && r > 0 && c > 0 && r < 3 && c < 3)) {
+			return true;
+		}
+		return false;
+	}
+
+	/*public void boardDisplay(char[][] board) {
+
+		for (int i = 0; i < ROW_SIZE; i++) {
+			for (int j = 0; j < COLUMN_SIZE; j++) {
+					
+				System.out.println("hello world");
+			}
+			//StdOut.printf("\n");
+			//StdOut.println("-----------");
+				
+		}
+	}
+
+	public static void main (String[] args) {
+
+		char [][] b = new char[3][3];
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				b[i][j] == 'a';
+			}
+		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.println("helloWorld");
+			}
+		}
+
+	}*/
+
 }
