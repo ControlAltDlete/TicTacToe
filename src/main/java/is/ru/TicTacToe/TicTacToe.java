230efb482ee787179448d3b44a48ec96d2eb1da6
Carlos RAgnar <carlosk12@ru.is>
2016-10-30 21:43:54 +0000

    Code comments inserted

--- src/main/java/is/ru/TicTacToe/TicTacToe.java
+++ src/main/java/is/ru/TicTacToe/TicTacToe.java
@@ -9,26 +9,34 @@ public class TicTacToe{
 	public static void main(String[] args) throws IOException{
 		System.out.println("Welcome to the TicTacToe game of the century!");
 
+		//Variables for the game defined.
+		Scanner input = new Scanner(System.in);
+		int move;
+		int moveForBoard[];
+		boolean player1Turn = true;
+
 		TicTacToe game = new TicTacToe();
 		Board gameBoard = new Board();
 		Player player1 = new Player("Player 1");
 		Player player2 = new Player("Player 2");
-		boolean player1Turn = true;
-		int move;
-		int moveForBoard[];
-		Scanner input = new Scanner(System.in);
 
+		//While loop that runs the game, it quits when somebody has won or there is a draw
 		while(!(gameBoard.gameIsWon('X') || gameBoard.gameIsWon('O')) && !(gameBoard.gameIsDrawn())){
+			//The board is displayed after each turn
 			gameBoard.displayBoard();
 
+			//Similar if-else statements, one for when it is Player 1's turn and the other one for when it's Player 2's turn.
 			if(player1Turn){
 				System.out.println("Player 2 move, choose a field to put an X: ");
 
+				//Move from the user read
 				move =  input.nextInt();
 				input.nextLine();
 
+				//Move chosen from the user translated for our two dimensional board.
 				moveForBoard = game.changeInput(move);
 
+				//The game will loop asking for a new input while the move chosen by the player is illegal, continues if the move is legal.
 				boolean legalMove = gameBoard.pickField(moveForBoard[0], moveForBoard[1], 'X'); 
 
 				while (!legalMove) {
@@ -42,18 +50,21 @@ public class TicTacToe{
 					legalMove = gameBoard.pickField(moveForBoard[0], moveForBoard[1], 'X');
 				}
 
-
+				//Boolean variable changed so the other player can make a move.
 			 	player1Turn = false;
 
 			}
 			else{
 				System.out.println("Player 1 move, choose a field to put an O: ");
 
+				//Move from the user read
 				move =  input.nextInt();
 				input.nextLine();
 
+				//Move chosen from the user translated for our two dimensional board.
 				moveForBoard = game.changeInput(move);
 
+				//The game will loop asking for a new input while the move chosen by the player is illegal, continues if the move is legal.
 				boolean legalMove = gameBoard.pickField(moveForBoard[0], moveForBoard[1], 'O');
 
 				while (!legalMove) {
@@ -67,14 +78,18 @@ public class TicTacToe{
 					legalMove = gameBoard.pickField(moveForBoard[0], moveForBoard[1], 'O');
 				}
 
+				//Boolean variable changed so the other player can make a move.
 				player1Turn = true;
 
 			}
 		}
+
+		//Board is displayed in it's end game state.
 		System.out.println();
 		gameBoard.displayBoard();
 	}
 
+	//Move by the user translated from a number 1 to 9 to indexes in our two dimensional array.
 	public int[] changeInput(int move){
 		int[] returnMove = new int[2];
 		switch(move){
