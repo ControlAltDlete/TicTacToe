9bb1c43bc3559b1d4c680721ffe44a5b61e26ff8
astabergs <astab14@ru.is>
2016-10-30 17:31:10 +0000

    commented the function in Board.java

--- src/main/java/is/ru/TicTacToe/Board.java
+++ src/main/java/is/ru/TicTacToe/Board.java
@@ -18,6 +18,7 @@ public class Board {
 		}	
 	}
 
+	//return true if no field has been picked else return false.
 	public boolean isEmpty() {
 		
 		for (int i = 0; i < ROW_SIZE; i++) {
@@ -31,6 +32,7 @@ public class Board {
 		return true;
 	}
 
+	//return true if the move is  legal, that is if the player picks a empty field inside the board, else return false.
 	public boolean checkIfMoveIsLegal(int r, int c) {
 		
 		if (r >= 0 && c >= 0 && r < 3 && c < 3 && board[r][c] == ' ') {
@@ -39,6 +41,7 @@ public class Board {
 		return false;
 	}
 	
+	//If the player picks emtpy field puts X or O on the field and return true, else return false.
 	public boolean pickField(int r, int c, char Symbol){
 		if(checkIfMoveIsLegal(r,c)){
 			board[r][c] = Symbol;
@@ -50,6 +53,7 @@ public class Board {
 		}
 	}
 
+	//return true if the game is won, else return false.
 	public boolean gameIsWon(char symbol) {
 		if(board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol ||
 				board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol ||
@@ -65,6 +69,7 @@ public class Board {
 		return false;
 	}
 
+	//returns true if no field on the board is empty.
 	public boolean boardIsFull() {
 
 		for (int i = 0; i < ROW_SIZE; i ++) {
@@ -78,6 +83,8 @@ public class Board {
 		return true;
 	}
 
+
+	//returns true if the game is tie (the board is full and the game is not won), else return false.
 	public boolean gameIsDrawn() {
 
 		if (boardIsFull() && !(gameIsWon('X') || gameIsWon('O'))) {
@@ -87,6 +94,7 @@ public class Board {
 		return false;
 	}
 
+	//Prints out the Board.
 	public void displayBoard() {
 
 		for (int i = 0; i < ROW_SIZE; i++) {
